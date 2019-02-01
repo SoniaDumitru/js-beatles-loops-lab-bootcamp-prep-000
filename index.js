12efdfa6170c109487f4ac5ff64d3b590597d568
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-01 18:27:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
   var array=[];
   for (let i=0; i < arrayOfMusicians.length; i++) {
     array.push(arrayOfMusicians[i] + " plays " + arrayOfInstruments[i])
-    }
   }
   return array;
 }
