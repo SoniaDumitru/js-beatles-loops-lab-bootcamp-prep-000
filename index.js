c30d266985344ccf282af31dedd704acabb58dea
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-01 18:21:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
   var aray=[];
   for (let i=0; i < arrayOfMusicians.length; i++) {
     array = `${arrayOfMusicians[i]} plays ${arrayOfInstruments[i]}`
-    
+
   }
   return array;
 }
-
