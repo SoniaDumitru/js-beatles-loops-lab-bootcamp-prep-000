48421fd223943531acc2ca8b4638769ce563051f
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-01 18:23:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var arrayOfMusicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var arrayOfInstruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
   var aray=[];
   for (let i=0; i < arrayOfMusicians.length; i++) {
